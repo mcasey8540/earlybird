10d701ea2e4a09318ed4168ad85f6fe565021b4c
Michael Casey <mcasey8540@gmail.com>
2016-03-31 13:06:01 -0400

    Add upvote function for comments and posts

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -20,17 +20,29 @@ app.factory('posts', ['$http', function($http){
 
 	o.upvote = function(post){
 		return $http.put('/posts/'+ post._id +'/upvote')
-			.success(function(data){
+			.success(function(res){
 				post.upvotes += 1;
 		});
 	};
 
-	o.get = function(post){
-		return $http.get('/posts/' + post._id)
-			.success(function(data){
-
+	o.get = function(id){
+		return $http.get('/posts/' + id)
+			//res param represents success
+			.then(function(res){
+				return res.data;
 			});
-	}
+	};
+
+	o.addComment = function(id, comment) {
+		return $http.post('/posts/' + id + '/comments', comment);
+	};
+
+	o.upvoteComment = function(id, comment){
+		return $http.put('/posts/' + id + '/comments/' + comment._id + '/upvote')
+			.success(function(data){
+				comment.upvotes += 1;
+			});
+	};
 
 	return o;
 
@@ -61,19 +73,27 @@ app.controller('MainCtrl', ['$scope', 'posts',
 	}
 ]);
 
-app.controller('PostsCtrl', ['$scope','$stateParams','posts',
-	function($scope, $stateParams, posts){
-		$scope.post = posts.posts[$stateParams.id];
+app.controller('PostsCtrl', ['$scope','posts','post',
+	function($scope, posts, post){
+		$scope.post = post;
 
 		$scope.addComment = function(){
 			if($scope.body === '') { return;}
-			$scope.post.comments.push({
+			
+			//add comment to post
+			posts.addComment(post._id, {
 				body: $scope.body,
-				author: 'user',
-				upvotes: 0
+				author: 'Mike',
+			}).success(function(comment){
+				$scope.post.comments.push(comment);
 			});
 			$scope.body = '';
-		}
+		};
+
+		$scope.incrementUpvotes = function(comment){
+			posts.upvoteComment(post._id,comment);
+		};
+
 }]);
 
 app.config([
@@ -97,8 +117,13 @@ app.config([
 			.state('posts', {
 				url: '/posts/{id}',
 				templateUrl: '/posts.html',
-				controller: 'PostsCtrl'
-			})
+				controller: 'PostsCtrl',
+				resolve: {
+					post: ['$stateParams', 'posts', function($stateParams, posts){
+						return posts.get($stateParams.id);
+					}]
+				}
+			});
 
 		$urlRouterProvider.otherwise('home');
 		
